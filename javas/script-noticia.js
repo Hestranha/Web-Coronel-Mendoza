7ab787b650df5c156020e9821c223a8f053efdf5
Hestranha <94768439+Hestranha@users.noreply.github.com>
2023-07-09 11:54:04 -0500

    Update script-noticia.js

--- javas/script-noticia.js
+++ javas/script-noticia.js
@@ -1,62 +1,129 @@
-
 function capitalizarTexto(texto) {
     return texto.toLowerCase().replace(/(^|\s)\S/g, function (letra) {
-      return letra.toUpperCase();
+        return letra.toUpperCase();
     });
 }
-function nComentario() {
-    let li = document.createElement("li");
 
+function mostrarComentarios() {
+    let comentariosContainer = document.getElementById("comentarios");
+    comentariosContainer.innerHTML = "";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "databases/obtener_comentarios.php", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let comentarios = JSON.parse(xhr.responseText);
+
+            // Iterar sobre los comentarios en orden inverso
+            for (let i = 0; i < comentarios.length; i++) {
+                let comentario = comentarios[i];
+                let li = document.createElement("li");
+                let contenido = document.createElement("p");
+
+                contenido.innerHTML = "<strong>" + comentario.nombre + " | " + comentario.fecha + "</strong><br>" + comentario.mensaje;
+                li.className = "comentario";
+                li.appendChild(contenido);
+                // Agregar el nuevo comentario al inicio de la lista
+                comentariosContainer.insertBefore(li, comentariosContainer.firstChild);
+            }
+        }
+    };
+    xhr.send();
+}
+
+function nComentario() {
     let nombre = document.getElementById("nombre").value;
     let correo = document.getElementById("correo").value;
     let comentario = document.getElementById("nuevoComentario").value;
-
-    if (comentario === '') {
-        alert("Ingrese un comentario!");
+    var nombreRegex = /^[a-zA-Z\s]*$/;
+    var correoRegex = /^\S+@\S+\.\S+$/;
+    if (comentario === "" || nombre === "" || correo === "") {
+        Swal.fire({
+            icon: "info",
+            title: "Oops...",
+            text: "Rellene los campos antes de enviar un comentario.",
+        });
+        return;
+    }
+    if (!nombreRegex.test(nombre)) {
+        mensaje =
+            "El campo Nombre solo debe contener letras y espacios.";
+        mensaje_Adv(mensaje);
+        return;
+    }
+    if (!correoRegex.test(correo)) {
+        mensaje = "Ingrese un Correo electrónico válido.";
+        mensaje_Adv(mensaje);
+        return;
+    }
+    if (comentario === "") {
+        mensaje =
+            "El campo Mensaje debe tener contenido.";
+        mensaje_Adv(mensaje);
+        return;
+    }
+    if (comentario === "" || nombre === "" || correo === "") {
+        alert("Complete los campos");
     } else {
         let fechaActual = new Date();
-        let hora = fechaActual.getHours().toString().padStart(2, '0'); // Agrega un cero antes de un solo dígito en la hora
-        let minutos = fechaActual.getMinutes().toString().padStart(2, '0'); // Agrega un cero antes de un solo dígito en los minutos
-
-        let dia = fechaActual.getDate().toString().padStart(2, '0'); // Agrega un cero antes de un solo dígito en el día
-        let mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0'); // Agrega un cero antes de un solo dígito en el mes
+        let hora = fechaActual.getHours().toString().padStart(2, "0");
+        let minutos = fechaActual.getMinutes().toString().padStart(2, "0");
+        let dia = fechaActual.getDate().toString().padStart(2, "0");
+        let mes = (fechaActual.getMonth() + 1).toString().padStart(2, "0");
         let año = fechaActual.getFullYear();
-        let fechaFormateada = dia + '/' + mes + '/' + año + ' - ' + hora + ':' + minutos;
-        
-        // Capitalizar la primera letra de cada palabra en el campo "nombre"
-    nombre = capitalizarTexto(nombre);
+        let fechaFormateada =
+            año + "-" + mes + "-" + dia;
+        // dia + "/" + mes + "/" + año + " - " + hora + ":" + minutos;
 
-    // Capitalizar solo la primera letra del mensaje
-    comentario = comentario.charAt(0).toUpperCase() + comentario.slice(1);
-        
-        let contenido = document.createElement("p");
-        contenido.innerHTML = "<strong>" + nombre + " - " + fechaFormateada + "</strong><br>" + comentario;
-        li.appendChild(contenido);
+        nombre = capitalizarTexto(nombre);
+        comentario = comentario.charAt(0).toUpperCase() + comentario.slice(1);
 
-        // Agregar el nuevo comentario al inicio de la lista
-        let comentariosContainer = document.getElementById("comentarios");
-        comentariosContainer.insertBefore(li, comentariosContainer.firstChild);
+        var datos = new FormData();
+        datos.append("nombre", nombre);
+        datos.append("correo", correo);
+        datos.append("comentario", comentario);
 
-        // Desplazarse automáticamente al inicio del contenedor
-        //comentariosContainer.scrollTop = 0;
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "databases/guardar_comentario.php", true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Mostrar el nuevo comentario primero
+                let comentariosContainer = document.getElementById("comentarios");
+                let li = document.createElement("li");
+                let contenido = document.createElement("p");
+
+                contenido.innerHTML =
+                    "<strong>" + nombre + " | " + fechaFormateada + "</strong><br>" + comentario;
+                li.className = "comentario";
+                li.appendChild(contenido);
+                comentariosContainer.insertBefore(li, comentariosContainer.firstChild);
+
+                limpiar();
+                //alert(xhr.responseText);
+            }
+        };
+        xhr.send(datos);
+        Swal.fire({
+            icon: "success",
+            title: "Comentario enviado",
+            text: "Su comentario se envio exitosamente.",
+        });
     }
+}
 
+
+function limpiar() {
     document.getElementById("nombre").value = "";
     document.getElementById("correo").value = "";
     document.getElementById("nuevoComentario").value = "";
-    li.className = "comentario";
-    /*
-    let borrar = document.createElement("p");
-    borrar.innerHTML = ("Borrar");
-    borrar.className = "close";
-    li.appendChild(borrar);
-
-    let close = document.getElementsByClassName("close");
-    for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let div = this.parentElement;
-            div.style.display = "none";
-        };
-    }
-    */
 }
+
+function mensaje_Adv(mensaje_dv) {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: mensaje_dv,
+    });
+}
+
+mostrarComentarios();
